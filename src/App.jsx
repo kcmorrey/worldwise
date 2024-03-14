4a70a5fbbfbbbeb9682744c9fdcd4e6d42501b7c
Kendra Morrey <kendra@piefi.io>
2024-03-13 17:03:59 -0700

    set up initial components, pages, and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import AppLayout from './pages/AppLayout';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
@@ -8,9 +10,16 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route index element={<Homepage />} />
 				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
+				<Route path="login" element={<Login />} />
+				<Route path="app" element={<AppLayout />}>
+					<Route index element={<p>LIST</p>} />
+					<Route path="cities" element={<p>List of cities</p>} />
+					<Route path="countries" element={<p>List of countries</p>} />
+					<Route path="form" element={<p>Form</p>} />
+				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
