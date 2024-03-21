f7c0bb8abe20cdfe49709700b94fa0d254fb482b
Kendra Morrey <kendra@piefi.io>
2024-03-20 17:24:59 -0700

    add map and form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import Form from './components/Form';
 import City from './components/City';
 import AppLayout from './pages/AppLayout';
 import Homepage from './pages/Homepage';
@@ -42,11 +43,11 @@ function App() {
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
-					<Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+					<Route index element={<Navigate replace to="cities" />} />
 					<Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
 					<Route path="cities/:id" element={<City />} />
 					<Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-					<Route path="form" element={<p>Form</p>} />
+					<Route path="form" element={<Form />} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import styles from './City.module.css';
 
 const formatDate = (date) =>
@@ -11,6 +11,10 @@ const formatDate = (date) =>
 
 function City() {
 	const { id } = useParams();
+	const [searchParams, setSearchParams] = useSearchParams();
+	const lat = searchParams.get('lat');
+	const lng = searchParams.get('lng');
+
 	// TEMP DATA
 	const currentCity = {
 		cityName: 'Lisbon',
@@ -20,7 +24,14 @@ function City() {
 	};
 
 	const { cityName, emoji, date, notes } = currentCity;
-	return <h1>City Component!{id}</h1>;
+	return (
+		<>
+			<h1>City Component!{id} </h1>
+			<p>
+				Position: {lat}, {lng}
+			</p>
+		</>
+	);
 	//TODO: add real component
 
 	// return (
