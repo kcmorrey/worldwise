3802d9d29bd3dba082079c4a398a71d3257f2b04
Kendra Morrey <kendra@piefi.io>
2024-04-04 16:20:25 -0700

    implement lazy loading of routes

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from 'react';
+import { useCallback } from 'react';
 import { useContext } from 'react';
 import { createContext, useState, useEffect } from 'react';
 
@@ -50,18 +51,21 @@ function CitiesProvider({ children }) {
 		fetchCities();
 	}, []);
 
-	async function getCity(id) {
-		if (Number(id) === currentCity.id) return;
-		dispatch({ type: 'loading' });
+	const getCity = useCallback(
+		async function (id) {
+			if (Number(id) === currentCity.id) return;
+			dispatch({ type: 'loading' });
 
-		try {
-			const res = await fetch(`${BASE_URL}/cities/${id}`);
-			const data = await res.json();
-			dispatch({ type: 'currentCity/loaded', payload: data });
-		} catch (error) {
-			dispatch({ type: 'rejected', payload: 'There was an error loading the city...' });
-		}
-	}
+			try {
+				const res = await fetch(`${BASE_URL}/cities/${id}`);
+				const data = await res.json();
+				dispatch({ type: 'currentCity/loaded', payload: data });
+			} catch (error) {
+				dispatch({ type: 'rejected', payload: 'There was an error loading the city...' });
+			}
+		},
+		[currentCity.id],
+	);
 
 	async function createCity(city) {
 		dispatch({ type: 'loading' });
